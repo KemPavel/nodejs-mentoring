25161ba360f61ce430709f4077f414e0b2c5d913
KemPavel <pkazakov18@gmail.com>
2019-11-28 16:23:27 +0800

    add task 1.2

---
+++ task2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+const file = './data/task2.csv';
+
+const errorHandler = (err) => {
+  console.log(err);
+};
+
+const writeLine = (data) => {
+  const formattedString = `${JSON.stringify(data)}\n`;
+  fs.appendFile('./data/task2_output.txt', formattedString, 'utf8', (err) => {
+    if (err) errorHandler(err);
+  });
+};
+
+csv()
+  .fromFile(file)
+  .subscribe((json) => {
+    return new Promise((resolve)=>{
+      resolve(json);
+    }).then(result => writeLine(result));
+  }, errorHandler);
